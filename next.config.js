49a6244b84d68ee72e6c2d8828f021ed16a63490
Samuel-Clover <you@example.com>
2021-09-16 19:43:03 -0300

    Portifolio beta

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    env: {
+      API_KEY:"AIzaSyCVyx5vDx4KEGvGuF1_QWc0yVKxFS0LgEY",
+      AUTH_DOMAIN:"devwebandgames.firebaseapp.com",
+      PROJECT_ID:"devwebandgames",
+      STORAGE_BUCKED:"devwebandgames.appspot.com",
+      MESSAGIN_SENDER_ID:"837234956383",
+      API_ID:"1:837234956383:web:fbfabfbae7957e98aa63ea",
+      AUTH_ADMINISTRATOR: "Samuel R M Kruszynski"
+    },
+  }
